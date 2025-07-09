df38fe9667e3b314b2816d09b2c2833baeb1f50c
Haashim Alvi <haashimalvi@pm.me>
2025-07-09 17:39:35 +0100

    Feat: Add test case for /posts endpoint to verify DB_HOST and data

--- test/test-server.js
+++ test/test-server.js
@@ -25,16 +25,26 @@ describe('Homepage', function() {
   });
 });
 
-// describe('Blog', function() {
-//   it('should display the list of posts at /posts GET', function(done) {
-//     chai.request(server)
-//       .get('/posts')
-//       .end(function(err, res){
-//         res.should.have.status(200);
-//         done();
-//       });
-//   });
-// });
+describe('Blog Posts', function() {
+  it('should display a list of 100 posts at /posts GET', function(done) {
+    chai.request(server)
+      .get('/posts')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json; // Assert that the response is JSON
+        res.body.should.be.an('array'); // Assert that the body is an array
+        res.body.length.should.be.eql(100); // Assert that there are 100 posts
+
+        // Optionally, check properties of the first post
+        if (res.body.length > 0) {
+          res.body[0].should.have.property('title');
+          res.body[0].should.have.property('content');
+          res.body[0].should.have.property('author');
+        }
+        done();
+      });
+  });
+});
 
 describe('Fibonacci', function() {
   it('should display the correct fibonacci value at /fibonacci/10 GET', function(done) {
